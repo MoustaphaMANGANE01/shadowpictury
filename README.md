194c7d79fc63e5aa12998c43c4dbc5ba6a80180c
TaphaLaye <manganetapha1994@gnail.com>
2021-07-10 15:43:24 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# What is this ?
+
+Get the perfects shadows every time for the non-designer
+
+# Installation
+
+`npm i shadowpicture --save`
+
+Then...
+
+```
+import { shadowpicture } from 'shadowpicture'
+
+shadowpicture({
+  shadow_type: 'soft'
+  padding:false
+});
+
+```
+## Options
+Shadowpicture supports 2 options,both of which are optionnal:
+
+* *shadow_type* - _hard_ | _soft_ (Defaults to soft)
+* *padding* - _boolean_  (Defaults to false)
---
+++ index.js
@@ -0,0 +1,23 @@
+function shadowpicture(options) {
+
+    let images = document.querySelectorAll('.shadowpicture')
+
+    if (options.shadow_type === "hard")
+
+        options.shadow_type = '10px'
+
+    else
+        options.shadow_type = '0px'
+
+
+    images.forEach((image) => {
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+
+        if (options.padding) {
+            image.style.padding = '1em';
+        }
+
+    })
+}
+
+module.exports.shadowpicture = shadowpicture
